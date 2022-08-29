b9164f4c7a498f72b60360c25fdd0725e895da7a
Nickhenry784 <nhiennguyen1605@gmail.com>
2022-08-29 15:41:28 +0700

    add turn

--- lib/src/widget/HomePage.dart
+++ lib/src/widget/HomePage.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:helloworld/src/widget/BuyPage.dart';
 import 'package:helloworld/src/widget/PlayPage.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 
 
@@ -12,23 +13,41 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  final Future<SharedPreferences> _prefs = SharedPreferences.getInstance();
+  late Future<int> _counter;
 
+  Future<void> _deducrementCounter() async {
+    final SharedPreferences prefs = await _prefs;
+    final int counter = (prefs.getInt('counter') ?? 10) - 1;
 
+    setState(() {
+      _counter = prefs.setInt('counter', counter).then((bool success) {
+        return counter;
+      });
+    });
+  }
+
+  final List<Items> entries = <Items>[
+    Items(1, const AssetImage('lib/src/assets/images/basketthrowingtechniques.png'), const AssetImage('lib/src/assets/images/3.png'), 'Basket Throwing Techniques'),
+    Items(2, const AssetImage('lib/src/assets/images/LEADINGTHEFLAG.png'), const AssetImage('lib/src/assets/images/2.png'), 'Leading The Flag'),
+    Items(3, const AssetImage('lib/src/assets/images/SHOULDSHOULDVATTHECHALLENGE.png'), const AssetImage('lib/src/assets/images/6.png'), 'Should Should V At The Challenge'),
+    Items(4, const AssetImage('lib/src/assets/images/STUNNINGTECHNIQUESATTHECHAIN.png'), const AssetImage('lib/src/assets/images/1.png'), 'Stunning Techniques At The Chain'),
+    Items(5, const AssetImage('lib/src/assets/images/targetimage.png'), const AssetImage('lib/src/assets/images/5.png'), 'Target'),
+    Items(6, const AssetImage('lib/src/assets/images/defense.png'), const AssetImage('lib/src/assets/images/4.png'), 'Defense'),
+  ];
+  
+  @override
+  void initState() {
+    super.initState();
+    _counter = _prefs.then((SharedPreferences prefs) {
+        return prefs.getInt('counter') ?? 10;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     double width = MediaQuery. of(context). size. width ;
     double height = MediaQuery. of(context). size. height;
-
-    final List<Items> entries = <Items>[
-      Items(1, const AssetImage('lib/src/assets/images/basketthrowingtechniques.png'), const AssetImage('lib/src/assets/images/3.png'), 'Basket Throwing Techniques'),
-      Items(2, const AssetImage('lib/src/assets/images/LEADINGTHEFLAG.png'), const AssetImage('lib/src/assets/images/2.png'), 'Leading The Flag'),
-      Items(3, const AssetImage('lib/src/assets/images/SHOULDSHOULDVATTHECHALLENGE.png'), const AssetImage('lib/src/assets/images/6.png'), 'Should Should V At The Challenge'),
-      Items(4, const AssetImage('lib/src/assets/images/STUNNINGTECHNIQUESATTHECHAIN.png'), const AssetImage('lib/src/assets/images/1.png'), 'Stunning Techniques At The Chain'),
-      Items(5, const AssetImage('lib/src/assets/images/targetimage.png'), const AssetImage('lib/src/assets/images/5.png'), 'Target'),
-      Items(6, const AssetImage('lib/src/assets/images/defense.png'), const AssetImage('lib/src/assets/images/4.png'), 'Defense'),
-    ];
-    
 
     return Scaffold(
       body: Container(
@@ -48,23 +67,42 @@ class _MyHomePageState extends State<MyHomePage> {
             children: [
               SizedBox(
                 height: height * 0.15,
-                child: Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        crossAxisAlignment: CrossAxisAlignment.center,
-                        children: [
-                          IconButton(
-                            icon: Image.asset('lib/src/assets/images/eye.png'),
-                            iconSize: 50,
-                            onPressed: () {
-                              Navigator.push(
-                                context,
-                                MaterialPageRoute(builder: (context) => const BuyPage()),
-                              );
-                            },
-                          ),
-        
-                        ],
-                  ),
+                child: FutureBuilder<int>(
+                  future: _counter,
+                  builder: (BuildContext context,AsyncSnapshot<int> snapshot) {
+                    if(snapshot.hasData){
+                      return Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            crossAxisAlignment: CrossAxisAlignment.center,
+                            children: [
+                              SizedBox(
+                                width: width * 0.2,
+                                child: Row(
+                                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                                  crossAxisAlignment: CrossAxisAlignment.center,
+                                  children: [
+                                    IconButton(
+                                      icon: Image.asset('lib/src/assets/images/eye.png'),
+                                      iconSize: 50,
+                                      onPressed: () {
+                                        Navigator.push(
+                                          context,
+                                          MaterialPageRoute(builder: (context) => const BuyPage()),
+                                        );
+                                      },
+                                    ),
+                                    Text(snapshot.data.toString()),
+                                  ],
+                                ),
+                              ),
+
+                            ],
+                        );
+                    }else{
+                      return const CircularProgressIndicator();
+                    }
+                  }
+                ),
               ),
               SizedBox(
                 height: height * 0.15,
@@ -82,11 +120,13 @@ class _MyHomePageState extends State<MyHomePage> {
                   itemBuilder: (BuildContext context, int index) {
                     return GestureDetector(
                       onTap: (() => {
+                        
                         Navigator.pushNamed(
                           context,
                           PlayPage.routeName,
                           arguments: ScreenArguments(entries[index].background,entries[index].name),
-                        )
+                        ),
+                        _deducrementCounter()
                       }),
                       child: SizedBox(
                         height: height * 0.1,
